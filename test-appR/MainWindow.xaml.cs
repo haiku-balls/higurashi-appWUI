580d03ada52a2595b2a76e44a909edd5653c9917
haiku-balls <haiku_real@proton.me>
2023-10-23 16:24:36 -0700

    Add project files.

---
+++ test-appR/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+using Microsoft.UI.Xaml;
+using test_appR;
+using Microsoft.UI;           // Needed for WindowId
+using Microsoft.UI.Windowing; // Needed for AppWindow
+using WinRT.Interop;          // Needed for XAML/HWND interop   
+using System;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace test_appR
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private AppWindow m_AppWindow;
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            m_AppWindow = GetAppWindowForCurrentWindow();
+                var titleBar = m_AppWindow.TitleBar;
+                // Hide default title bar.
+                titleBar.ExtendsContentIntoTitleBar = true;
+                Title = "Higurashi Wiki App";
+                // m_AppWindow.SetIcon("");
+        }
+
+        // SYSTEM FUNCTION, GRABS THE CURRENT WINDOW. SEE MAINWINDOW.
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+
+        // From Lovely Reminders (Should handle navi)
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(typeof(homePage));
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                // Settings page.
+                contentFrame.Navigate(typeof(settingPage));
+            } 
+            else
+            {
+                // Everything else.
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag.ToString())
+                {
+                    case "home":
+                        contentFrame.Navigate(typeof(homePage));
+                        break;
+                    case "keiichi_nav":
+                        contentFrame.Navigate(typeof(Char.keiichi));
+                        break;
+                    case "rena_nav":
+                        contentFrame.Navigate(typeof(Char.rena));
+                        break;
+                }
+            }
+        }
+
+        private void NavView_PaneClosed(NavigationView sender, object args)
+        {
+            NavView.IsPaneOpen = true;
+        }
+    }
+}
